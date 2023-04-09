e30593a4dcb399cbbc2839050086042e92add3ab
Praful932 <praful.mohanan@gmail.com>
2023-04-09 17:26:38 +0530

    First code commit

---
+++ test_pkg.py
@@ -0,0 +1,2 @@
+from packaging.tags import platform_tags;
+print(list(platform_tags()))
---
+++ test_pytorch.py
@@ -0,0 +1,5 @@
+import torch
+# this ensures that the current MacOS version is at least 12.3+
+print(torch.backends.mps.is_available())
+# this ensures that the current current PyTorch installation was built with MPS activated.
+print(torch.backends.mps.is_built())
