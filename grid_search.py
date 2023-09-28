6c321641394105760c03cbce2df5f499e1f1796c
Praful932 <praful.mohanan@gmail.com>
2023-09-28 16:35:20 +0530

    Add build files

--- grid_search.py
+++ grid_search.py
@@ -64,12 +64,9 @@ rouge_metric = evaluate.load("rouge")
 def postprocess_text(preds, labels):
     preds = [pred.strip() for pred in preds]
     labels = [label.strip() for label in labels]
-
-
     # rougeLSum expects newline after each sentence
     preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
     labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
-
     return preds, labels
 
 
