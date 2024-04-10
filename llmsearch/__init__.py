83dac248b211760dd095f6a8ed83b94b3d3b75ca
praful932 <praful.mohanan@gmail.com>
2024-04-10 19:38:11 +0000

    Add test reqs

--- llmsearch/__init__.py
+++ llmsearch/__init__.py
@@ -1,4 +1,4 @@
 from llmsearch.patches.transformers_monkey_patch import hijack_samplers
 
-# print("Monkey Patching .generate function of `transformers` library")
-# hijack_samplers()
+print("Monkey Patching .generate function of `transformers` library")
+hijack_samplers()
