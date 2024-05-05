6fd699d587f2b5657f611138f445c3d4e62640f0
praful932 <praful.mohanan@gmail.com>
2024-05-05 05:44:38 +0000

    log samsum state

--- archive/runpod_dev_env_setup.sh
+++ archive/runpod_dev_env_setup.sh
@@ -20,5 +20,8 @@ pip install torch@https://download.pytorch.org/whl/cu121/torch-2.2.0%2Bcu121-cp3
 # if using awq
 # pip install autoawq==0.2.4 autoawq_kernels==0.0.6
 
+# if using gptq
+pip install auto_gptq==0.7.1
+
 # if using exllama
 # pip install exllamav2@https://github.com/turboderp/exllamav2/releases/download/v0.0.14/exllamav2-0.0.14+cu121-cp310-cp310-linux_x86_64.whl
